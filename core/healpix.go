3f487f19df8eb3994fb5357494829596212983f7
shoulinwei <weishoulin@astrolab.cn>
2023-12-28 20:25:38 +0800

    hp bug

--- core/healpix.go
+++ core/healpix.go
@@ -13,7 +13,6 @@ import (
 	"math"
 )
 
-// service 服务
 type healpix struct {
 	nSide int32
 	order string `default:"ring"` // ring or nested
@@ -26,12 +25,15 @@ func (s *healpix) Npix() int64 {
 func (s *healpix) PixelNsideValid(pixel int64) bool {
 	return pixel >= 0 && pixel < s.Npix()
 }
+
 func (s *healpix) NestedToRing(nested_index int64) int64 {
 	return int64(C.healpixl_nested_to_xy(C.long(nested_index), C.int(s.nSide)))
 }
+
 func (s *healpix) RingToNested(ring_index int64) int64 {
 	return int64(C.healpixl_ring_to_xy(C.long(ring_index), C.int(s.nSide)))
 }
+
 func (s *healpix) RaDecDegToHealpix(ra float64, dec float64) int64 {
 	hpixId := C.radec_to_healpixl(C.double(Deg2Rad(ra)), C.double(Deg2Rad(dec)), C.int(s.nSide))
 	if s.order == "ring" {
